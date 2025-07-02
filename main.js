ab371c7ad9e3a408ad6791267c892b406d1442f4
rodrigue malemba <rmalemba28@gmail.com>
2025-07-02 14:24:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const generator = (word) => {
-    
+const wordGen = () => {
+    const wordS = ['flyer', 'flier', 'fliers', 'flyers', 'flying', 'flyings', 'flyer\'s', 'flier\'s', 'flyers\'s', 'fliers\'s'];
+    const word = Math.floor(Math.random() * wordS.length);
+    return wordS[word]
 }
+console.log(wordGen());
+// This function generates a random word from the predefined list and logs it to the console.
+// The words include variations of "flyer" and "flier" in different forms.
